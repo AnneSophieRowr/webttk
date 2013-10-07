05819dddb7079ebe6a9e82a44363e4fb919006ca
annesophie <annesophie@annesophie-alien.(none)>
2013-10-07 14:29:48 +0200

    decorator

--- app/decorators/device_decorator.rb
+++ app/decorators/device_decorator.rb
@@ -1,28 +1,18 @@
 class DeviceDecorator < Draper::Decorator
   delegate_all
 
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
-  #   def created_at
-  #     helpers.content_tag :span, class: 'time' do
-  #       object.created_at.strftime("%a %m/%d/%y")
-  #     end
-  #   end
-
-	def virtual_test
+	def virtual_detailed
+		content = []
 		if object.hypervisor.nil? && !object.vms.empty?
-			[h.content_tag(:dt, I18n.t('devices.vms')),
-			h.content_tag(:dd, object.vms.each {|vm| h.link_to vm.name, vm})].join(' ')
-			#h.content_tag :dd
-			#	objects.vms.each do |vm|
-			#		link_to vm.name, vm
-			#	end
-			#end].join(' ')
+			content << h.content_tag(:dt, I18n.t('devices.vms'))
+			object.vms.each do |vm|
+				content << h.content_tag(:dd, h.link_to(vm.name, vm))
+			end
 		elsif !object.hypervisor.nil?
-			h.content_tag(:dt, I18n.t('devices.hypervisor_id'))
-			h.content_tag(:dd, object.hypervisor.name)
+			content << [h.content_tag(:dt, I18n.t('devices.hypervisor_id'))]
+			content << h.content_tag(:dd, object.hypervisor.name)
 		end
+		content.join(' ').html_safe
 	end
 
 end
