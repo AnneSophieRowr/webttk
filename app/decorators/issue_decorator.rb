8a91db3a274c637d6060d158814e87f3598c8263
annesophie <annesophie@annesophie-alien.(none)>
2014-02-05 12:28:59 +0100

    issues style

--- app/decorators/issue_decorator.rb
+++ app/decorators/issue_decorator.rb
@@ -3,7 +3,7 @@ class IssueDecorator < Draper::Decorator
 
   def actions_show
     actions = object.acts.collect.with_index do |action, idx|
-      [h.content_tag(:dt, "Action \##{idx + 1}"), h.content_tag(:dd, action.description), h.content_tag(:dd, action.date), h.content_tag(:dd, action.user.decorate.name)].join(' ')
+      [h.content_tag(:dt, h.link_to("Action \##{idx + 1}", h.edit_issue_act_path(action.issue, action))), h.content_tag(:dd, action.description), h.content_tag(:dd, "#{action.date} - #{action.user.decorate.name}")].join(' ')
     end
     return actions.join(' ').html_safe
   end
