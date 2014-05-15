ce74cfe10ba47255e0223c28b0862e20d0f447cf
annesophie <annesophie@annesophie-alien.(none)>
2014-05-15 17:29:57 +0200

    214 235 240 241

--- app/assets/javascripts/issues.js
+++ app/assets/javascripts/issues.js
@@ -3,7 +3,9 @@ $(document).ready(function(){
   $('#issue_status_id').on('change', function() {
     if ($(this).val() == 4) {
       $('#issue_closure_date').val(moment().format('Do/MM/YYYY'));
+      $('#issue_closure_date').prop('readonly', true);
       $('#issue_closure_time').val(moment().format('HH:mm'));
+      $('#issue_closure_time').prop('readonly', true);
     }
   });
 
